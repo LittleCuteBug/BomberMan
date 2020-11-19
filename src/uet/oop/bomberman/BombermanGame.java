abafba678dd30ceb6706395cdfc91df61991ff5e
quanghuy-redmibook <quanghuynguyen236@gmail.com>
2020-11-19 22:23:45 +0700

    Add Game with simple loadMap

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -7,34 +7,41 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+import uet.oop.bomberman.entities.item.BombItem;
+import uet.oop.bomberman.entities.item.FlameItem;
+import uet.oop.bomberman.entities.item.SpeedItem;
+import uet.oop.bomberman.entities.movingObject.Balloom;
 import uet.oop.bomberman.entities.movingObject.Bomber;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.movingObject.Oneal;
+import uet.oop.bomberman.entities.tile.Brick;
 import uet.oop.bomberman.entities.tile.Grass;
+import uet.oop.bomberman.entities.tile.Portal;
 import uet.oop.bomberman.entities.tile.Wall;
 import uet.oop.bomberman.graphics.Sprite;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BombermanGame extends Application {
-    
-    public static final int WIDTH = 20;
-    public static final int HEIGHT = 15;
-    
+
     private GraphicsContext gc;
     private Canvas canvas;
-    private List<Entity> entities = new ArrayList<>();
-    private List<Entity> stillObjects = new ArrayList<>();
-
-
+    private Game game;
     public static void main(String[] args) {
         Application.launch(BombermanGame.class);
     }
 
     @Override
     public void start(Stage stage) {
+        game = new Game();
+        game.loadMap("res/levels/Level1.txt");
+        //createMap();
         // Tao Canvas
-        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
+
+        canvas = new Canvas(Sprite.SCALED_SIZE * game.getWIDTH(), Sprite.SCALED_SIZE * game.getHEIGHT());
         gc = canvas.getGraphicsContext2D();
 
         // Tao root container
@@ -48,43 +55,17 @@ public class BombermanGame extends Application {
         stage.setScene(scene);
         stage.show();
 
+
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long l) {
-                render();
-                update();
+                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                game.render(gc);
+                game.update();
             }
         };
         timer.start();
-
-        createMap();
-
-        Entity bomberman = new Bomber(1.5, 3.2);
-        entities.add(bomberman);
     }
 
-    public void createMap() {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
-                Entity object;
-                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-                    object = new Wall(i, j);
-                }
-                else {
-                    object = new Grass(i, j);
-                }
-                stillObjects.add(object);
-            }
-        }
-    }
 
-    public void update() {
-        entities.forEach(Entity::update);
-    }
-
-    public void render() {
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        stillObjects.forEach(g -> g.render(gc));
-        entities.forEach(g -> g.render(gc));
-    }
 }
---
+++ src/uet/oop/bomberman/Game.java
@@ -0,0 +1,136 @@
+package uet.oop.bomberman;
+
+import javafx.scene.canvas.GraphicsContext;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.item.BombItem;
+import uet.oop.bomberman.entities.item.FlameItem;
+import uet.oop.bomberman.entities.item.SpeedItem;
+import uet.oop.bomberman.entities.movingObject.Balloom;
+import uet.oop.bomberman.entities.movingObject.Bomber;
+import uet.oop.bomberman.entities.movingObject.Oneal;
+import uet.oop.bomberman.entities.tile.Brick;
+import uet.oop.bomberman.entities.tile.Grass;
+import uet.oop.bomberman.entities.tile.Portal;
+import uet.oop.bomberman.entities.tile.Wall;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Game {
+
+    private int WIDTH = 31;
+    private int HEIGHT = 13;
+    private int level;
+
+    private List<Entity> enemy = new ArrayList<>();
+    private List<Entity> item = new ArrayList<>();
+    private List<Entity> wall = new ArrayList<>();
+    private List<Entity> brick = new ArrayList<>();
+    private List<Entity> glass = new ArrayList<>();
+    private Bomber bomber;
+    private Portal portal;
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public int getWIDTH() {
+        return WIDTH;
+    }
+
+    public void createMap() {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                Entity object;
+                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
+                    object = new Wall(i, j);
+                    wall.add(object);
+                }
+                else {
+                    object = new Grass(i, j);
+                    glass.add(object);
+                }
+            }
+        }
+        bomber = new Bomber(1,1);
+        portal = new Portal(WIDTH-2,HEIGHT-2);
+    }
+
+    void loadMap(String filePath){
+        enemy.clear();
+        item.clear();
+        wall.clear();
+        brick.clear();
+        glass.clear();
+        try {
+            Scanner reader = new Scanner(new File(filePath));
+            level = reader.nextInt();
+            HEIGHT = reader.nextInt();
+            WIDTH = reader.nextInt();
+            reader.nextLine();
+            for(int j=0;j<HEIGHT;j++)
+            {
+                String line = reader.nextLine();
+                System.out.println(line);
+                for(int i = 0;i<WIDTH;i++)
+                {
+                    char c = line.charAt(i);
+                    switch (c) {
+                        case '#':
+                            wall.add(new Wall(i,j));
+                            break;
+                        case '*':
+                            brick.add(new Brick(i,j));
+                            break;
+                        case 'x':
+                            portal = new Portal(i,j);
+                            break;
+                        case 'p':
+                            bomber = new Bomber(i,j);
+                            break;
+                        case '1':
+                            enemy.add(new Balloom(i,j));
+                            break;
+                        case '2':
+                            enemy.add(new Oneal(i,j));
+                            break;
+                        case 'b':
+                            item.add(new BombItem(i,j));
+                            break;
+                        case 'f':
+                            item.add(new FlameItem(i,j));
+                            break;
+                        case 's':
+                            item.add(new SpeedItem(i,j));
+                            break;
+                    }
+                    glass.add(new Grass(i,j));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        //glass.forEach(Entity::update);
+        //wall.forEach(Entity::update);
+        brick.forEach(Entity::update);
+        item.forEach(Entity::update);
+        portal.update();
+        bomber.update();
+        enemy.forEach(Entity::update);
+    }
+
+    public void render(GraphicsContext gc) {
+        glass.forEach(g -> g.render(gc));
+        wall.forEach(g -> g.render(gc));
+        brick.forEach(g -> g.render(gc));
+        item.forEach(g -> g.render(gc));
+        portal.render(gc);
+        bomber.render(gc);
+        enemy.forEach(g -> g.render(gc));
+    }
+}
