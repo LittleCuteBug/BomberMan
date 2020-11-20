260612b700dce65d9c65044995cd7b8868b17005
Phạm Anh Cường <phamanhcuongvna2511@gmail.com>
2020-11-20 17:25:44 +0700

    Add demo update Action and Image of Balloom

--- src/uet/oop/bomberman/entities/movingObject/Balloom.java
+++ src/uet/oop/bomberman/entities/movingObject/Balloom.java
@@ -1,7 +1,11 @@
 package uet.oop.bomberman.entities.movingObject;
 
 import uet.oop.bomberman.Game;
+import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.input.Keyboard;
+
+import java.util.Random;
 
 public class Balloom extends MovingEntity {
     public Balloom(double x, double y, Game game) {
@@ -9,6 +13,95 @@ public class Balloom extends MovingEntity {
     }
 
     public void update() {
-        // random di chuyển đến khi nào chạm wall hoặc brick thì quay lại?
+        updateAction();
+        updateImage();
+    }
+
+    private void updateAction(){
+        Random generator = new Random();
+        int rand = generator.nextInt(4) + 1;
+        switch (rand) {
+            case 1:
+                if (canMoveUp()) {
+                    moveUp();
+                } /*else if (canMoveDown()) {
+                    moveDown();
+                } else if (canMoveLeft()) {
+                    moveLeft();
+                } else if (canMoveRight()) {
+                    moveRight();
+                }*/
+            case 2:
+                if (canMoveDown()) {
+                    moveDown();
+                } /*else if (canMoveLeft()) {
+                    moveLeft();
+                } else if (canMoveRight()) {
+                    moveRight();
+                } else if (canMoveUp()) {
+                    moveUp();
+                }*/
+            case 3:
+                if (canMoveLeft()) {
+                    moveLeft();
+                } else {
+                    rand = generator.nextInt(4) + 1;
+                }
+                /*else if (canMoveRight()) {
+                    moveRight();
+                } else if (canMoveUp()) {
+                    moveUp();
+                } else if (canMoveDown()) {
+                    moveDown();
+                }*/
+            case 4:
+                if (canMoveRight()) {
+                    moveRight();
+                } /*else if (canMoveUp()) {
+                    moveUp();
+                } else if (canMoveDown()) {
+                    moveDown();
+                } else if (canMoveLeft()) {
+                    moveLeft();
+                }*/
+        }
+    }
+
+    private void updateImage(){
+        imgStage = imgStage%3;
+        switch (direction){
+            case UP:
+                img = Sprite.balloom_left1.getFxImage();
+                break;
+            case DOWN:
+                img = Sprite.balloom_left1.getFxImage();
+                break;
+            case LEFT:
+                switch (imgStage) {
+                    case 0:
+                        img = Sprite.balloom_left1.getFxImage();
+                        break;
+                    case 1:
+                        img = Sprite.balloom_left2.getFxImage();
+                        break;
+                    case 2:
+                        img = Sprite.balloom_left3.getFxImage();
+                        break;
+                }
+                break;
+            case RIGHT:
+                switch (imgStage){
+                    case 0:
+                        img = Sprite.balloom_right1.getFxImage();
+                        break;
+                    case 1:
+                        img = Sprite.balloom_right2.getFxImage();
+                        break;
+                    case 2:
+                        img = Sprite.balloom_right3.getFxImage();
+                        break;
+                }
+                break;
+        }
     }
 }
