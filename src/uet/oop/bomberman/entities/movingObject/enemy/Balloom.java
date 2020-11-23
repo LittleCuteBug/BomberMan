6a76fe9335d9bfaec401ee46aea74931af2a3d57
quanghuy-redmibook <quanghuynguyen236@gmail.com>
2020-11-23 10:11:21 +0700

    Enemy class grouped

---
+++ src/uet/oop/bomberman/entities/movingObject/enemy/Balloom.java
@@ -0,0 +1,37 @@
+package uet.oop.bomberman.entities.movingObject.enemy;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.Game;
+import uet.oop.bomberman.entities.movingObject.Enemy;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Balloom extends Enemy {
+    private static final Image[] spriteLeft= {Sprite.balloom_left1.getFxImage(),Sprite.balloom_left2.getFxImage(),Sprite.balloom_left3.getFxImage()};
+    private static final Image[] spriteRight= {Sprite.balloom_right1.getFxImage(),Sprite.balloom_right2.getFxImage(),Sprite.balloom_right3.getFxImage()};
+
+    public Balloom(double x, double y, Game game) {
+        super(x, y, game, Balloom.spriteRight[0], 150, 0, 0,0);
+    }
+
+    private void updateImage(){
+        imgStage = imgStage%3;
+        switch (direction){
+            case LEFT:
+            case DOWN:
+                img = Balloom.spriteLeft[imgStage];
+                break;
+            case RIGHT:
+            case UP:
+                img = Balloom.spriteRight[imgStage];
+                break;
+        }
+    }
+
+    public void update() {
+        updateImage();
+        if (!isRemoved()) {
+            updateAction();
+            updateObject();
+        }
+    }
+}
