9373a17129e51c6a544d37fc10052f8cc9725b4e
Quang Huy Nguyen - PC <quanghuynguyen236@gmail.com>
2020-12-12 14:42:26 +0700

    Add Pontan

--- src/uet/oop/bomberman/Map.java
+++ src/uet/oop/bomberman/Map.java
@@ -217,7 +217,7 @@ public class Map {
             int x = random.nextInt(WIDTH);
             int y = random.nextInt(HEIGHT);
             if (!cellUsed[x][y]) {
-                int type = random.nextInt(Math.min(game.getLevel()+1,7));
+                int type = random.nextInt(Math.min(game.getLevel()+1,8));
                 switch (type){
                     case 0:
                         enemy.add(new Balloom(x,y,game));
@@ -239,6 +239,9 @@ public class Map {
                         break;
                     case 6:
                         enemy.add(new Pass(x,y, game));
+                        break;
+                    case 7:
+                        enemy.add(new Pontan(x,y,game));
                         break;
                 }
                 cntEnemy++;
--- src/uet/oop/bomberman/entities/movingObject/enemy/Pontan.java
+++ src/uet/oop/bomberman/entities/movingObject/enemy/Pontan.java
@@ -2,15 +2,30 @@ package uet.oop.bomberman.entities.movingObject.enemy;
 
 import javafx.scene.image.Image;
 import uet.oop.bomberman.Game;
-import uet.oop.bomberman.entities.movingObject.Enemy;
+import uet.oop.bomberman.entities.movingObject.SmartEnemy;
 import uet.oop.bomberman.graphics.Sprite;
 
-public class Pontan extends Enemy {
+import java.util.Random;
+
+public class Pontan extends SmartEnemy {
     private static final Image[] spriteLeft= {Sprite.pontan_left1.getFxImage(),Sprite.pontan_left2.getFxImage(),Sprite.pontan_left3.getFxImage()};
     private static final Image[] spriteRight= {Sprite.pontan_right1.getFxImage(),Sprite.pontan_right2.getFxImage(),Sprite.pontan_right3.getFxImage()};
     private static final Image[] spriteDead = {Sprite.pontan_dead.getFxImage(),Sprite.mob_dead1.getFxImage(),Sprite.mob_dead2.getFxImage(),Sprite.mob_dead3.getFxImage()};
     public Pontan(double x, double y, Game game) {
-        super(x, y, game, Pontan.spriteRight[0], 90, 0, 0,0);
+        super(x, y, game, Pontan.spriteRight[0], 80, 2, 0,1,3);
+        timeBetweenPlaceBomb = 1000;
+        setBombPassUsed();
+        setFlamePassUsed();
+        setBrickPassUsed();
+    }
+
+    @Override
+    protected void updateAction() {
+        super.updateAction();
+        Random random = new Random();
+        if(random.nextDouble()<0.005) {
+            placeBomb();
+        }
     }
 
     protected Image getSpriteLeft(int imgStage){
